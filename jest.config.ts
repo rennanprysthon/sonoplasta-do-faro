87b738c00fb415db002f3c7b10fd07a687194b95
Filipe Melo <filipe.melo@liferay.com>
2023-01-12 09:35:11 -0300

    chore: add new dependencies & fix configuration files

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
 export default {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
+  preset: "ts-jest",
+  moduleFileExtensions: ["js", "json", "ts"],
+  testRegex: ".*\\.spec\\.ts$",
   bail: 1,
   clearMocks: true,
   coverageProvider: "v8",
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
+  coverageDirectory: "./coverage",
+  testEnvironment: "node",
 };
